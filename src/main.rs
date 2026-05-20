f582ce0e96cd733add2a42fbeb4bd808f435b0a8
yenuldasanayake <161976898+yenuldasanayake@users.noreply.github.com>
2026-05-20 19:10:02 +0530

    processing a guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
+
+
+    let mut guess = String::new();
+    let apples = 5;
+
     println!("Hello, world!");
-}
+    println!("guess the number");
+    
+    let x = 5;
+    let y = 10;
+
+    println!("x = {x} and y + 2 = {}", y + 2);
+
+    //guesssing game
+    println!("Guess the number");
+    println!("Please input your guess");
+
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Faild to read line");
+
+    println!("You guessed: {guess}")
+
+    }
